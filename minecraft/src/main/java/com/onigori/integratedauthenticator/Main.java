f4921e9a5f6bba792af806d3eafabbb436359ff9
Onigori_Rage <frosted.onigori2023@gmail.com>
2026-02-13 23:55:11 +0900

    first commit

---
+++ minecraft/src/main/java/com/onigori/integratedauthenticator/Main.java
@@ -0,0 +1,17 @@
+package com.onigori.integratedauthenticator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
